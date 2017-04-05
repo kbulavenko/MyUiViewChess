2f184010e05b130ad0058898ee32332e78c947e0
kbulavenko <kbulavenko@gmail.com>
2017-04-05 12:15:43 +0300

    Initial Commit

---
+++ MyUiViewChess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyUiViewChess
+//
+//  Created by  Z on 05.04.17.
+//  Copyright © 2017 ItStep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyUiViewChess/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyUiViewChess
+//
+//  Created by  Z on 05.04.17.
+//  Copyright © 2017 ItStep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
